b30dfd573ee1ca0abf3e1f6e3cd35fdfdfaa667a
C2iscool <codkagamine07@gmail.com>
2023-02-17 23:06:06 -0800

    return of functions

--- 011-functions/main.js
+++ 011-functions/main.js
@@ -1,14 +1,16 @@
 //Functions are fun!
 
 function myMath (a, b) {
-    
-    console.log(`Result is ${a + b}`)
-    console.log(`Result is ${a - b}`)
-    console.log(`Result is ${a * b}`)
-    console.log(`Result is ${a / b}`)
+    let result = a * b
+    return result 
+    // console.log(`Result is ${a + b}`)
+    // console.log(`Result is ${a - b}`)
+    // console.log(`Result is ${a * b}`)
+    // console.log(`Result is ${a / b}`)
 }
 
-myMath(7, 7)
+const result = myMath(7, 7)
+console.log(`function result returned as ${result}`)
 
 //functions may also be created by declaring a constant variable as like this:
 //const myMath = () {
