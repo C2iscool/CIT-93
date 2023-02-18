6f6b7673bd05653f60d2d26d92b0aff98141508b
C2iscool <codkagamine07@gmail.com>
2023-02-17 18:46:37 -0800

    more on variables

--- 010-typeof/scripts/main.js
+++ 010-typeof/scripts/main.js
@@ -1,4 +1,4 @@
-let testSubjectOne = 'Milk' // testSubjectOne is a string
+let testSubjectOne = '7' // testSubjectOne is a string
 console.log('testSubjectOne is Milk which is typeof', (typeof testSubjectOne))
 
 let testSubjectTwo = 7 // testSubjectTwo is a number
@@ -6,3 +6,23 @@ console.log('testSubjectTwo is 7 which is typeof', (typeof testSubjectTwo))
 
 let testSubjectThree = true // testSubjectThree is a boolean
 console.log('testSubjectThree is true which is typeof', (typeof testSubjectThree))
+
+console.log(testSubjectTwo == testSubjectOne) // "==" is the abstract equality operator, which will do a type conversion
+console.log(testSubjectTwo === testSubjectOne) // "===" is the strict equality operator, which does not do a type conversion
+
+let chrisVar
+console.log(`chrisVar is typeof ${typeof chrisVar}`) //chrisVar is "undefined" because a value has not been assigned to it
+
+let chrisVarTwo = null
+console.log(`chrisVarTwo is typeof ${typeof chrisVarTwo}`) //chrisVarTwo is "null"/"object" because it has been intentionally set to this null value
+
+const chrisConst = 77
+
+//if (false) {
+//    const chrisConst = 77
+//    console.log(chrisConst)
+//} else {
+//    
+//}
+
+//do not use var 
