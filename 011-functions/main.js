4878de11688586539fdc29d0361715eb2c11a5d6
C2iscool <codkagamine07@gmail.com>
2023-02-17 22:50:37 -0800

    functions are fun

---
+++ 011-functions/main.js
@@ -0,0 +1,16 @@
+//Functions are fun!
+
+function myMath (a, b) {
+    
+    console.log(`Result is ${a + b}`)
+    console.log(`Result is ${a - b}`)
+    console.log(`Result is ${a * b}`)
+    console.log(`Result is ${a / b}`)
+}
+
+myMath(7, 7)
+
+//functions may also be created by declaring a constant variable as like this:
+//const myMath = () {
+//
+//}
