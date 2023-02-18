4ecc4f50429188a93263c198b523f77599808e5f
C2iscool <codkagamine07@gmail.com>
2023-02-17 23:14:23 -0800

    my functions

--- 011-functions/main.js
+++ 011-functions/main.js
@@ -12,6 +12,13 @@ function myMath (a, b) {
 const result = myMath(7, 7)
 console.log(`function result returned as ${result}`)
 
+function ozToCups (a) {
+    let result = a / 8
+    return result
+}
+
+console.log(`${ozToCups(1)}`)
+
 //functions may also be created by declaring a constant variable as like this:
 //const myMath = () {
 //
