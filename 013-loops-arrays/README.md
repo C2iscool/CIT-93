c78178997f6c7bd4cc37f7194829398f339e6af4
Christopher Acosta <codkagamine07@gmail.com>
2023-03-08 21:45:25 -0800

    refactor and feedback

--- 013-loops-arrays/README.md
+++ 013-loops-arrays/README.md
@@ -1,1 +1,5 @@
 # I have not learned much more about dev tools than I already knew. I do not really use them much as of now. Should I? That is probably a maybe and most likely a must later on.
+
+# For the refactor, it made sense to me. It made sense and did reduce the amount of lines while stil retaining readability and functionality.
+
+# P.S. I apologize for the lack of activity from me. I was busy doing some stuff for people's birthdays. I hope to catch up on these assignments ASAP. Also as a heads up in case you need it, I will be gone most of next week for traveling and will be back some time in the week after. I thank you ver much for keeping the class structured the way it is right now. It allows me to much more easily do these things.
--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -1,5 +1,5 @@
-const myMPG = []
-const myTripCost = []
+const MY_MPG = []
+const MY_TRIP_COST = []
 
 const updateDOM = (input) => {
     divEl = document.querySelector('#output')
@@ -12,27 +12,23 @@ const trackMPGandCost = (mi = 10, gal = 1, cost = 2) => {
     const MPG = Math.round(mi/gal)
     const tripCost = Math.round(gal * cost)
     updateDOM(`MPG: ${MPG} Trip Cost: ${tripCost}`)
-    myMPG.push(MPG)
-    myTripCost.push(tripCost)
+    MY_MPG.push(MPG)
+    MY_TRIP_COST.push(tripCost)
 }
 
-const calcAvgMPG = () => {
-    let totalMPG = 0
-    for(let i = 0; i < myMPG.length; i++) {
-        totalMPG = totalMPG + myMPG[i]
+const calcSUM = (arr) => {
+    let sum = 0
+    for(let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
     }
-    let avgMPG = Math.round(totalMPG/myMPG.length)
-    updateDOM(`Average MPG is ${avgMPG}`)
+    return sum
 }
-
-//code challenge
-
-const calcAvgTripCost = () => {
-    let totalTripCost = 0
-    for (let i = 0; i < myTripCost.length; i++) {
-        totalTripCost = totalTripCost + myTripCost[i]
-    }
-    let avgTripCost = Math.round(totalTripCost/myTripCost.length)
+const calcAvg = () => {
+    let sumMPG = calcSUM(MY_MPG)
+    let sumTripCost = calcSUM(MY_TRIP_COST)
+    let avgMPG = Math.round(sumMPG/MY_MPG.length)
+    let avgTripCost = Math.round(sumTripCost/MY_TRIP_COST.length)
+    updateDOM(`Average MPG: ${avgMPG}`)
     updateDOM(`Average Trip Cost: ${avgTripCost}`)
 }
 
@@ -42,5 +38,4 @@ trackMPGandCost(200, 11, 3)
 trackMPGandCost(200, 9, 5)
 trackMPGandCost(100, 12, 2)
 trackMPGandCost(100, 18, 4)
-calcAvgMPG()
-calcAvgTripCost()
+calcAvg()
