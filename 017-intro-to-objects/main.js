2e5b08b8412d2abcbc187a53d71a999144e7327a
Christopher Acosta <codkagamine07@gmail.com>
2023-04-27 23:03:43 -0700

    this is mine

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -15,12 +15,24 @@ const myObject = {
         os: 'iOS 16'
     },
     myGreeting: function(person) {
-        
         return `${this.name} who loves ${this.hobbies}, says: Heyo ${person}!`
+    },
+
+    myPetAction: function () {
+        return `${this.name} started petting ${myDog.name}, who loves ${myDog.hobbies}`
     }
+}
+
+const myDog = {
+    name: 'Maya',
+    age: 13,
+    hobbies: ['going on walks', 'sleeping', 'playing with toys']
 }
 
 const greeting = myObject.myGreeting('Rio')
 console.log(greeting)
+
+const petAction = myObject.myPetAction()
+console.log(petAction)
 
 //const myObjectTwo = new Object (part of another potential way to implement objects.)
