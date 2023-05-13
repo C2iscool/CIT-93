709cda5ba2f871be76188569fa67a47ca2964c50
Christopher Acosta <codkagamine07@gmail.com>
2023-05-13 15:46:30 -0700

    student subclass

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -1,3 +1,3 @@
 # Week 13 Intro to OOP
 
-## I do see the difference. We are sort of creating a base of an object that we can call and plug our own things into.
+## I do see the difference. We are sort of creating a base of an object that we can call and plug our own things into. The classes and sub classes are making a bit more sense now. I am still unsure how exactlywe might implement this later in larger code.
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -9,7 +9,17 @@ class Person { // When creating classes, Pascal casing is used rather than Camel
     }
 }
 
-const chris = new Person("Chris", "Acosta", "1337 Anonymous Dr")
+class Student extends Person {
+    constructor(firstName, lastName, address, classList) {
+        super(firstName, lastName, address)
+        this.classList = classList
+    }
+    showStatus() {
+        console.log(`${this.firstName} lives at ${this.address} and is taking ${this.classList}`)
+    }
+}
+
+const chris = new Student("Chris", "Acosta", "1337 Anonymous Dr", ["PE17B", "CIT93"])
 chris.showStatus()
 const walter = new Person("Walter", "White", "308 Negra Arroyo Lane")
 walter.showStatus()
