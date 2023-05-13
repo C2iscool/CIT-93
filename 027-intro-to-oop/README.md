55462bca0ba49cba539c388eb4cbad58eb60dbfa
Christopher Acosta <codkagamine07@gmail.com>
2023-05-13 16:55:15 -0700

    my attempt at oop coding challenge

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -1,3 +1,5 @@
 # Week 13 Intro to OOP
 
 ## I do see the difference. We are sort of creating a base of an object that we can call and plug our own things into. The classes and sub classes are making a bit more sense now. I am still unsure how exactlywe might implement this later in larger code.
+
+### I got the getters and setters to work properly.
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,21 +1,45 @@
 class Person { // When creating classes, Pascal casing is used rather than Camel casing. e.g. "ChrisAcosta"
     constructor(firstName, lastName, address){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._address = address
+    }
+    getfirstName() {
+        return `${this._firstName}`
+    }
+    setFirstName(newFirstName) {
+        this._firstName = newFirstName
+    }
+    getLastName() {
+        return `${this._lastName}`
+    }
+    setLastName(newLastName) {
+        this._lastName = newLastName
+    }
+    getAddress() {
+        return `${this._address}`
+    }
+    setAddress(newAddress) {
+        this._address = newAddress
     }
     showStatus() {
-        console.log(`${this.firstName} lives at ${this.address}`)
+        console.log(`${this.getfirstName()} lives at ${this.getAddress()}`)
     }
 }
 
 class Student extends Person {
     constructor(firstName, lastName, address, classList) {
         super(firstName, lastName, address)
-        this.classList = classList
+        this._classList = classList
+    }
+    getClassList() {
+        return `${this._classList}`
+    }
+    setClassList(newClassList) {
+        this._classList = newClassList
     }
     showStatus() {
-        console.log(`${this.firstName} lives at ${this.address} and is taking ${this.classList}`)
+        console.log(`${this.getfirstName()} lives at ${this.getAddress()} and is taking ${this.getClassList()}`)
     }
 }
 
@@ -23,3 +47,5 @@ const chris = new Student("Chris", "Acosta", "1337 Anonymous Dr", ["PE17B", "CIT
 chris.showStatus()
 const walter = new Person("Walter", "White", "308 Negra Arroyo Lane")
 walter.showStatus()
+
+console.log(chris.getfirstName())
