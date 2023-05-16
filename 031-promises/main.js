42279834bccb9c50e7a50154ef91b4bc075e06ac
Christopher Acosta <codkagamine07@gmail.com>
2023-05-15 23:57:55 -0700

    intro to promises

---
+++ 031-promises/main.js
@@ -0,0 +1,39 @@
+function getWeather() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve('Cloudy')
+        }, 100)
+    })
+}
+
+function getWeatherIcon(weather) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            switch(weather){
+                case 'Sunny':
+                    resolve('🌞')
+                    break
+                case 'Cloudy':
+                    resolve('☁️')
+                    break
+                case 'Rainy':
+                    resolve('🌧️')
+                    break
+                default:
+                    reject('NO ICON FOUND')
+            }
+        }, 100)
+    })
+}
+
+function onSuccess(data) {
+    console.log(`Success ${data}`)
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`)
+}
+
+getWeather()
+.then(getWeatherIcon)
+.then(onSuccess, onError)
