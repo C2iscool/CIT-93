ea64ce1cc55fa82247b032c1eaedf764e71923f0
Christopher Acosta <codkagamine07@gmail.com>
2023-05-16 21:36:51 -0700

    updated code

--- 033-photo-album/main.js
+++ 033-photo-album/main.js
@@ -1,14 +1,24 @@
-const output = document.getElementById('output')
-
-function updateDOM(message, el) {
-    const newEl = document.createElement(el)
-    newEl.textContent = message
-    output.appendChild(newEl)
-}
-async function photos() {
-fetch('https://jsonplaceholder.typicode.com/photos')
-        .then(response => response.json())
-        .then(json => updateDOM)
+async function updateDOM(photos) {
+    photos.forEach(img => {
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', img.thumbnailUrl);
+        document.getElementById('output').appendChild(imgEl);
+    });
 }
 
-photos()
+async function getPhotos() {
+    try {
+        const data = await fetch('https://jsonplaceholder.typicode.com/photos');
+        return await data.json();
+
+    } catch (error) {
+        console.log(`Error ${error}`);
+    }
+}
+
+async function start() {
+    const photos = await getPhotos();
+    await updateDOM(photos);
+}
+
+start();
